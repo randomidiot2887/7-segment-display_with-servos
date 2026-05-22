960b9e6b3a94ae482d76f832b389b0bc1f7a8b07
randomidiot2887 <aliaikmohammedshameem@gmail.com>
2026-05-22 09:56:43 +0500

    Finished the implementation of function display_num() which is used to display the digits that can be displayed by manupulating the servos accordingly

--- segmentdisplay/src/main.cpp
+++ segmentdisplay/src/main.cpp
@@ -20,13 +20,14 @@ const int NUM_SERVOS = 7;
 const int max_servo_open=180;
 // Constant that stores the serial baud to output serial at
 const int baud=9600;
-//
+// Constant that declares the delay between the switching of servos to try and reduce power draw
+const int time=100;
+
 // =======================================
-//
 // Section of code that contains functions and procedures required to run the code
-// 
+
 // Function that acts as the heartbeat of the arduino.
-// Can be used to identify the state of initialisation and stuff by just glancing at the arduino
+// Can be used to identify the state of initialisation and stuff bym just glancing at the arduino
 // Assists in allowing visual debugging during initialisation of arduino
 void blinkStatus(int times) {
   for (int i = 0; i < times; i++) {
@@ -35,6 +36,106 @@ void blinkStatus(int times) {
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
   }
+}
+
+// Function that is used to control the servos that spin at a time, handles displaying of numbers
+// Used to clean up loop()
+// syntax: display_num(number)
+// number must be 0 <= number < 10 to display a number on the 7 segment display
+// if number is 10, itll be equivelent to clearning the screen
+// if number is less then 0 or greater then 10, the procedure WILL BREAK
+void display_num(int num=10) {
+  // Case that handles the procedures the procedure will run depending on the input
+  switch (num) {
+    case 10:
+      // Resets the servo position of all of them to 0
+      // Basically is same as clearning the display of every value
+      for (int i=0; i<7; i++) {
+        servo_object[i].write(0);
+        delay(100);
+      }
+      break;
+    // Values that change the contents of the display
+    case 1:
+      // Displaying the number 1
+      // By enabling servos 2 and 6
+      servo_object[0].write(0); delay(time);              // Digit 7
+      servo_object[1].write(0); delay(time);              // Digit 7
+      servo_object[2].write(max_servo_open); delay(time); // Digit 7
+      servo_object[3].write(0); delay(time);              // Digit 7
+      servo_object[4].write(0); delay(time);              // Digit 7
+      servo_object[5].write(0); delay(time);              // Digit 7
+      servo_object[6].write(max_servo_open); delay(time); // Digit 7
+      break;
+    case 2:
+      // Displaying the number 2
+      // By enabling servos 1, 2, 3, 4 & 5
+      servo_object[0].write(max_servo_open); delay(time); // Digit 1
+      servo_object[1].write(max_servo_open); delay(time); // Digit 2
+      servo_object[2].write(max_servo_open); delay(time); // Digit 3
+      servo_object[3].write(max_servo_open); delay(time); // Digit 4
+      servo_object[4].write(max_servo_open); delay(time); // Digit 5
+      servo_object[5].write(0); delay(time);              // Digit 6
+      servo_object[6].write(0); delay(time);              // Digit 7
+      break;
+    case 3:
+      // Displaying the number 3
+      // By enabling servos 1, 2, 3, 5 & 6
+      servo_object[0].write(max_servo_open); delay(time); // Digit 1
+      servo_object[1].write(max_servo_open); delay(time); // Digit 2
+      servo_object[2].write(max_servo_open); delay(time); // Digit 3
+      servo_object[3].write(0); delay(time);              // Digit 4
+      servo_object[4].write(max_servo_open); delay(time); // Digit 5
+      servo_object[5].write(max_servo_open); delay(time); // Digit 6
+      servo_object[6].write(0); delay(time);              // Digit 7
+      break;
+    case 4:
+      // Displaying the number 4
+      // By enabling servos 2, 3, 6 & 7
+      servo_object[0].write(0); delay(time);              // Digit 1
+      servo_object[1].write(max_servo_open); delay(time); // Digit 2
+      servo_object[2].write(max_servo_open); delay(time); // Digit 3
+      servo_object[3].write(0); delay(time);              // Digit 4
+      servo_object[4].write(0); delay(time);              // Digit 5
+      servo_object[5].write(max_servo_open); delay(time); // Digit 6
+      servo_object[6].write(max_servo_open); delay(time); // Digit 7
+      break;
+    case 5:
+      // Displaying the number 5
+      // By enabling servos 1, 3, 5, 6 & 7
+      servo_object[0].write(max_servo_open); delay(time); // Digit 1
+      servo_object[1].write(0); delay(time);              // Digit 2
+      servo_object[2].write(max_servo_open); delay(time); // Digit 3
+      servo_object[3].write(0); delay(time);              // Digit 4
+      servo_object[4].write(max_servo_open); delay(time); // Digit 5
+      servo_object[5].write(max_servo_open); delay(time); // Digit 6
+      servo_object[6].write(max_servo_open); delay(time); // Digit 7
+      break;
+    case 6:
+      // Displaying the number 6
+      // By enabling servos 1, 3, 4, 5, 6 & 7
+      servo_object[0].write(max_servo_open); delay(time); // Digit 1
+      servo_object[1].write(0); delay(time);              // Digit 2
+      servo_object[2].write(max_servo_open); delay(time); // Digit 3
+      servo_object[3].write(max_servo_open); delay(time); // Digit 4
+      servo_object[4].write(max_servo_open); delay(time); // Digit 5
+      servo_object[5].write(max_servo_open); delay(time); // Digit 6
+      servo_object[6].write(max_servo_open); delay(time); // Digit 7
+      break;
+    case 7:
+      // Displaying the number 7
+      // By enabling servos 1, 2 & 6
+      servo_object[0].write(max_servo_open); delay(time); // Digit 1
+      servo_object[1].write(max_servo_open); delay(time); // Digit 2
+      servo_object[2].write(0); delay(time);              // Digit 3
+      servo_object[3].write(0); delay(time);              // Digit 4
+      servo_object[4].write(0); delay(time);              // Digit 5
+      servo_object[5].write(max_servo_open); delay(time); // Digit 6
+      servo_object[6].write(0); delay(time);              // Digit 7
+      break;
+  }
+  // Blinks the debug LED once to help with visual debugging
+  blinkStatus(1);
 }
 // =================================
 // Void setup
@@ -59,6 +160,9 @@ void setup() {
   blinkStatus(1);
 } 
 
+// Code that runs as a loop, repeating for as long as the arduino is running
+// Is used to toggle the servos to their desired positions
+// Dont mess with this after its coded. please?
 void loop() {
   // put your main code here, to run repeatedly:
 }
